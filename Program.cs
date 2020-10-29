63c7f10517e5cba5f32e0b099f7c3e98a5b41bdf
Veera Sudhir Bandi <sudhir.bandi@ggktech.com>
2020-10-29 08:54:22 +0530

    Optimized code

---
+++ Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace DevSample
+{
+    class Program
+    {
+        //Note: these settings should not be modified
+        //hopefully we have more than 1 core to work with, run cores/2 cycles with a max of 4 cycles;
+
+        static readonly int _cyclesToRun= Environment.ProcessorCount > 1 ? 
+                                          Environment.ProcessorCount / 2 > 4?4: Environment.ProcessorCount / 2 :
+                                          1; 
+        static readonly int _samplesToLoad = 222222;
+        static readonly DateTime _sampleStartDate= new DateTime(1990, 1, 1, 1, 1, 1, 1);
+        static readonly TimeSpan _sampleIncrement= new TimeSpan(0, 5, 0);
+        static readonly string _fileName= string.Format("DevSample_{0}.{1}", DateTime.Now.Ticks, "txt");
+
+        static void Main(string[] args)
+        {
+
+
+            Stopwatch totalMonitor = new Stopwatch();
+            totalMonitor.Start();
+
+            LogMessage($"Starting Execution on a {Environment.ProcessorCount} core system. A total of {_cyclesToRun} cycles will be run");
+
+
+            //sudhir: Used Parallel for to run multiple threads for the cpu cycles
+            Parallel.For(0, _cyclesToRun, i =>
+            {
+                try
+                {
+
+                    TimeSpan cycleElapsedTime;
+                    TimeSpan elapsedTiimeTillSampleLoad;
+
+                    Stopwatch cycleTimer = new Stopwatch();
+
+                    SampleGenerator sampleGenerator = new SampleGenerator(_sampleStartDate, _sampleIncrement);
+
+                    LogMessage($"Cycle {i} Started Sample Load.");
+
+
+                    cycleTimer.Start();
+
+                    sampleGenerator.LoadSamples(_samplesToLoad);
+
+                    cycleTimer.Stop();
+                    elapsedTiimeTillSampleLoad  = cycleElapsedTime = cycleTimer.Elapsed;
+                    LogMessage($"Cycle {i} Finished Sample Load. Load Time: {cycleElapsedTime.TotalMilliseconds.ToString("N")} ms.");
+                    LogMessage($"Cycle {i} Started Sample Validation.");
+
+
+                    cycleTimer.Restart();
+                    sampleGenerator.ValidateSamples();
+
+                    cycleTimer.Stop();
+                    cycleElapsedTime = cycleTimer.Elapsed;
+
+                    LogMessage($"Cycle {i} Finished Sample Validation. Total Samples Validated: {sampleGenerator.SamplesValidated}. Validation Time: {cycleElapsedTime.TotalMilliseconds.ToString("N")} ms.");
+
+                    float valueSum = 0;
+                    foreach (Sample s in sampleGenerator.Samples)
+                        valueSum += s.Value;
+
+                    //TODO: why do we only seem to get 7 digits of precision? The CEO wants to see at least 20!
+
+                    //Sudhir: When i ran the program i got a precision of more than 20 digits eg: 1,39,31,21,00,00,00,00,00,000.00.
+                    LogMessage($"Cycle {i} Sum of All Samples: {valueSum.ToString("N")}.");
+
+                    //Sudhir: Fixed the incorrect total cycle time
+                    LogMessage($"Cycle {i} Finished. Total Cycle Time: {(elapsedTiimeTillSampleLoad.TotalMilliseconds + cycleElapsedTime.TotalMilliseconds).ToString("N")} ms.");
+
+
+                }
+                catch (Exception ex)
+                {
+                    LogMessage($"Execution Failed!\n{ex.ToString()}");
+                }
+
+
+            });
+
+            totalMonitor.Stop();
+
+            LogMessage("-----");
+            LogMessage($"Execution Finished. Total Elapsed Time: {totalMonitor.Elapsed.TotalMilliseconds.ToString("N")} ms.");
+
+
+            Console.Read();
+
+        }
+
+
+
+        static void LogMessage(string message)
+        {
+
+            LogToFile(message);
+            Console.WriteLine($"{DateTime.Now.ToString("HH:mm:ss.fffff")} - {message}");
+        }
+
+        static void LogToFile(string message)
+        {
+            //TODO: implement this when someone complains about it not working... 
+            //everything written to the console should also be written to a log under C:\Temp. A new log with a unique file name should be created each time the application is run.
+
+            //sudhir: Implemented logging to a text file
+            try
+            {
+                using (FileStream objFilestream = new FileStream(string.Format("{0}\\{1}", "C:/TEMP/", _fileName), FileMode.Append, FileAccess.Write))
+                {
+                    StreamWriter objStreamWriter = new StreamWriter((Stream)objFilestream);
+                    objStreamWriter.WriteLine(message);
+                    objStreamWriter.Close();
+                    objFilestream.Close();
+                }
+            }
+            catch (Exception)
+            {
+                //
+            }
+        }
+    }
+}
---
+++ SampleGenerator.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DevSample
+{
+    class SampleGenerator
+    {
+
+
+        private readonly DateTime _sampleStartDate;
+        private readonly TimeSpan _sampleIncrement;
+
+        public SampleGenerator(DateTime sampleStartDate, TimeSpan sampleIncrement)
+        {
+            Samples = new List<Sample>();
+            _sampleStartDate = sampleStartDate;
+            _sampleIncrement = sampleIncrement;
+        }
+
+
+        /// <summary>
+        /// Samples should be a time-descending ordered list
+        /// </summary>
+        public List<Sample> Samples { get; }
+
+
+        public int SamplesValidated { get; private set; }
+
+
+        public void LoadSamples(int samplesToGenerate)
+        {
+
+            //TODO: can we load samples faster?
+
+            Samples.Clear();
+
+            DateTime date = _sampleStartDate;
+
+            for (int i = 0; i < samplesToGenerate; i++)
+            {
+                Sample s = new Sample(i == 0);
+                s.LoadSampleAtTime(date);
+
+                //_sampleList.Insert(0, s);
+                //sudhir: Used Add instead of Insert in the list. The complexity for insert is O(n) snice it needs to move all elements and for add it is O(1)
+                Samples.Add(s);
+                date += _sampleIncrement;
+            }
+
+
+        }
+
+
+        public void ValidateSamples()
+        {
+            //TODO: can we validate samples faster?
+
+            //Validating elements from the end since we are validaing the previous elements added in the list using Add()
+            int samplesValidated = 0;
+            for (int i = Samples.Count; i > 0; i--)
+            {
+                if (Samples[i - 1].ValidateSample(i > 1 ? Samples[i - 2] : null, _sampleIncrement)) //in this sample the ValidateSample is always true but assume that's not always the case
+                    samplesValidated++;
+            }
+
+            SamplesValidated = samplesValidated;
+        }
+
+    }
+}
+
